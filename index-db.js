fcaea2b6c9fc81fdc61ba3360696ee1f68d31257
Ranganathan <mailtodeepak88@gmail.com>
2020-04-11 22:58:09 -0400

    Git-Integ #comment jira integ

--- index-db.js
+++ index-db.js
@@ -26,7 +26,7 @@ app.get('/', function (req, res) {
 });
 
 
-// server to JIRA 2
+// server to JIRA 3
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
